e63550bd7d37bb3a56547413b0b9433eff49bd46
Owen Burbank <owen.burbank@4cinsights.com>
2017-12-27 21:00:36 -0800

    new bisect method optimize for area

--- redistrict.py
+++ redistrict.py
@@ -1,10 +1,10 @@
 import json
 from itertools import groupby
 
-def districts():
+def districts(n, bisect):
     data = json.load(open('/Users/ob4c/redistrict-bigfiles/wageo.json'))
     group = get_group(data)
-    groups = partition(group, 33)
+    groups = partition(group, n, bisect)
     new = label(data, groups)
     json.dump(new,open('new2.json','w'))
     return new
@@ -23,15 +23,18 @@ def get_coordinate_from_feature(feature):
     xs,ys = zip(*coordinates)
     return max(xs), max(ys)
 
-def divide(group, ratio):
+def divide(group, ratio, axis):
     population = get_pop(group)*ratio
-    axis = get_axis(group)
     group1 = []
     group2 = []
     new_population = 0
+    counter = 0
     for i,v in axis:
         if new_population < population:
             for block in v:
+                if counter == 0:
+                    print('b1', block['i'])
+                    counter += 1
                 group1.append(block)
                 new_population += block['p']
         else:
@@ -39,15 +42,15 @@ def divide(group, ratio):
                 group2.append(block)
     return group1, group2
 
-def partition(group, sections):
+def partition(group, sections, bisect):
     print('sections', sections)
     if sections == 1:
 	    return [group]
     low = sections//2
     print('low', low)
     ratio = float(low)/sections
-    group1, group2 = divide(group, ratio)
-    return partition(group1, low) + partition(group2,sections-low)
+    group1, group2 = bisect(group, ratio)
+    return partition(group1, low, bisect) + partition(group2,sections-low, bisect)
 
 def get_spread(data, key):
     a = sorted([i[key] for i in data])
@@ -69,7 +72,32 @@ def get_axis(group):
     return groupby(sorted(group,key=lambda x: x[dim]), key=lambda x: x[dim])
 
 def get_axes(group):
-    return groupby(sorted(group,key=lambda x: x['x']), key=lambda x: x['x']), groupby(sorted(group,key=lambda x: x['y']), key=lambda x: x['y'])
+    by_x = sorted(group,key=lambda x: x['x'])
+    by_y = sorted(group,key=lambda x: x['y'])
+    print('x', by_x[0])
+    print('y', by_y[0])
+    return groupby(by_x, key=lambda x: x['x']), groupby(by_y, key=lambda x: x['y'])
+
+def get_ratio(group):
+    spreads = sorted([get_spread(group, 'x'), get_spread(group, 'y')])
+    return spreads[0]/spreads[1]
+
+def naive_bisect(group, ratio):
+    axis = get_axis(group)
+    return divide(group, ratio, axis)
+
+def optimize_area_bisect(group, ratio):
+    a1, a2 = get_axes(group)
+    groupings1 = divide(group, ratio, a1)
+    min1 = min(map(get_ratio, groupings1))
+    print('min1', min1)
+    groupings2 = divide(group, ratio, a2)
+    min2 = min(map(get_ratio, groupings2))
+    print('min2', min2)
+    if min1 > min2:
+        return groupings1
+    else:
+        return groupings2
 
 def get_pop(group):
     return sum(i['p'] for i in group)
